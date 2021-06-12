d7f1a1a2628e6154288952ce4a10c1f1c78f364a
Ruchi Thakur <ruchiithakur910@gmail.com>
2021-06-12 14:23:53 +0530

    added code

--- Primenumber.java
+++ Primenumber.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 class Primenumber{
+    public void claculate(){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int num=scan.nextInt();
+        
+        for(int i=2;i<num;i++){
+            if(num%i==0){
+                System.out.println("Not a prime number.");
+                break;
+            }
+        }
+
+    }
     
 }
